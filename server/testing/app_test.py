a4c4fdbe6305aab6cefed257b9a29ab84b0c9f3d
Ben Botsford <ben.botsford@flatironschool.com>
2022-11-09 09:49:25 -0600

    update port, ul tags

--- server/testing/app_test.py
+++ server/testing/app_test.py
@@ -4,7 +4,7 @@ import re
 from app import app
 
 class TestApp:
-    '''Flask application in flask_app.py'''
+    '''Flask application in app.py'''
 
     def test_animal_route(self):
         '''has a resource available at "/animal/<id>".'''

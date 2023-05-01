26732f35d2ae455525b219a0099e659b808e1703
Ben Botsford <ben.botsford@flatironschool.com>
2023-05-01 09:48:17 -0500

    finish animal models tests

--- server/testing/models/animal_test.py
+++ server/testing/models/animal_test.py
@@ -1,5 +1,5 @@
-from app import app
-from server.models import db, Animal
+from app import app, db
+from server.models import Animal, Enclosure, Zookeeper
 
 class TestAnimal:
     '''Animal model in models.py'''
@@ -15,3 +15,33 @@ class TestAnimal:
         a = Animal(name='Phil', species='Rhinoceros')
         assert a.name == 'Phil'
         assert a.species == 'Rhinoceros'
+
+    def test_has_enclosure_id_and_zookeeper_id(self):
+        '''has foreign keys for enclosure_id and zookeeper_id.'''
+        a = Animal()
+        assert hasattr(a, 'enclosure_id')
+        assert hasattr(a, 'zookeeper_id')
+
+    def test_can_be_saved_to_database(self):
+        '''can be added to a transaction and committed to create a database record.'''
+        with app.app_context():
+            a = Animal()
+            db.session.add(a)
+            db.session.commit()
+            assert hasattr(a, 'id')
+            assert db.session.query(Animal).filter(Animal.id == a.id).one_or_none()
+
+    def test_is_related_to_enclosures_and_zookeepers(self):
+        '''has access to its associated enclosure and zookeeper objects.'''
+        with app.app_context():
+            a = Animal()
+            e = Enclosure()
+            z = Zookeeper()
+            db.session.add_all([a, e, z])
+            db.session.commit()
+            a.enclosure_id = e.id
+            a.zookeeper_id = z.id
+            db.session.add(a)
+            db.session.commit()
+            assert a.enclosure == e
+            assert a.zookeeper == z
